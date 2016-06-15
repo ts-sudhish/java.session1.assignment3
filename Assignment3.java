305279b6fc33aec8ec733543dbc530c9c05f7c03
ts-sudhish <sudhish.soss@gmail.com>
2016-06-15 13:24:10 +0000

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package sdh.java.session1;
+
+import java.util.Scanner;
+
+/**
+ * @author sudhi
+ * 
+ */
+public class Assignment3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a = (int) 12510.23;
+		System.out
+				.println("value of a = "
+						+ a
+						+ "\nhere since we are trying to store a float number in int, the decimal number is discarded");
+		a = '1';
+		System.out
+				.println("value of a = "
+						+ a
+						+ "\nhere since we are trying to store a char in int, the ascii value is saved into a");
+
+	}
+
+}
